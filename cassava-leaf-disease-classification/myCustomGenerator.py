2f79eda82379215a3ddd7980456e984d22ca5891
Ashery Mbilinyi <mbiash00@dmi-20-pc-29.dmi.unibas.ch>
2020-12-17 17:26:58 +0100

    kazi tu

---
+++ cassava-leaf-disease-classification/myCustomGenerator.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+
+from skimage.io import imread
+from skimage.transform import resize
+
+
+class CustomGenerator(tf.keras.utils.Sequence):
+
+    def __init__(self, image_filenames, labels, batch_size):
+        self.image_filenames = image_filenames
+        self.labels = labels
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(np.int)
+
+    def __getitem__(self, idx):
+        batch_x = self.image_filenames[idx * self.batch_size: (idx + 1) * self.batch_size]
+        batch_y = self.labels[idx * self.batch_size: (idx + 1) * self.batch_size]
+
+        return np.array([resize(imread('/home/mbiash00/kaggle/cassava-leaf-disease-classification/dataset/train_images/' + str(file_name)), (80, 80, 3))
+                         for file_name in batch_x]) / 255.0, np.array(batch_y)
--- cassava-leaf-disease-classification/training.py
+++ cassava-leaf-disease-classification/training.py
@@ -6,22 +6,23 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing import image
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from sklearn.utils import shuffle
 from tqdm import tqdm
+from myCustomGenerator import CustomGenerator
+
 
 def reading_data():
-    return pd.read_csv('cassava-leaf-disease-classification/train.csv')
+    return pd.read_csv('dataset/train.csv')
 
 
 def loading_images():
     train_image = []
     train = reading_data()
     for i in tqdm(range(train.shape[0])):
-        img = image.load_img('cassava-leaf-disease-classification/train_images/' + train['image_id'][i],
+        img = image.load_img('dataset/train_images/' + train['image_id'][i],
                              color_mode='rgb', target_size=None, interpolation='nearest')
         img = image.img_to_array(img)
         img = img / 255
@@ -33,7 +34,7 @@ def loading_images():
 
 
 def shuffle_data():
-    train = pd.read_csv('cassava-leaf-disease-classification/train.csv')
+    train = pd.read_csv('dataset/train.csv')
     filenames = train['image_id']
     filenames = filenames.to_numpy()
     labels = train['label'].values
@@ -44,22 +45,54 @@ def shuffle_data():
 
 #train test split
 
-def train_test():
+
+def train_val_generation(batch_size):
+    #batch_size = 32
     shuffled_filenames,shuffled_labels = shuffle_data()
-    X_train, X_test, y_train, y_test = train_test_split(shuffled_filenames, shuffled_labels, random_state=42, test_size=0.2)
-    print(X_train)
-    print(y_train)
+    X_train, X_val, y_train, y_val = train_test_split(shuffled_filenames, shuffled_labels,
+                                                      random_state=42, test_size=0.2)
+    training_batch_generator = CustomGenerator(X_train, y_train, batch_size)
+    validation_batch_generator = CustomGenerator(X_val, y_val, batch_size)
+    # print(X_train.shape)
+    # print(X_val.shape)
+
+    return training_batch_generator,validation_batch_generator
+
+def creating_model():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(80, 80, 3)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(5, activation='softmax'))
+
+    return model
 
 
 def main():
-    train_test()
+    batch_size = 32
+    train_batch_generator,val_batch_generator = train_val_generation(batch_size)
+    model = creating_model()
+    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    model.summary()
+
+    model.fit(train_batch_generator,
+                        steps_per_epoch=int(17117 // batch_size),
+                        epochs=10,
+                        verbose=1,
+                        validation_data=val_batch_generator,
+                        validation_steps=int(4280 // batch_size))
+    #train_val_generation()
 
 
 
 if __name__ == '__main__':
-    print("dealing with main")
+    print("start to learn")
     main()
-    print("finish dealing with main")
+    print("finished learning")
 
 
 
